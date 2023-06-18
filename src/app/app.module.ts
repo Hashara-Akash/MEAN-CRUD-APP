6be9f3930dece1e21454f8039000d54f0e513e4f
Hashara Withanage <g10022.hashara@gmail.com>
2023-06-18 14:05:01 +0530

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { EmployeesComponent } from './employees/employees.component';
-import { EmployeeFormComponent } from './employees/employee-form/employee-form.component';
+
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    EmployeesComponent,
-    EmployeeFormComponent,
+    
+   
 
   ],
   imports: [
